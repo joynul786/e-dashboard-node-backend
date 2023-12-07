5de35d5f64073e6c27ac2caa4afa8581ee30c15b
sjana98 <sjana8901@gmail.com>
2023-12-07 21:49:47 +0530

    little changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const userModel = require("./mongoDBConnect/UserSchema");
 require("./mongoDBConnect/config");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, resp) => {
     const userData = await userModel.insertMany(req.body);
