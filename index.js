d516c90be2c50048df1953a12bb368ceae591e77
sjana98 <sjana8901@gmail.com>
2023-12-24 19:21:11 +0530

    variable names change and read note

--- index.js
+++ index.js
@@ -6,40 +6,42 @@ require("./mongoDBConnect/config");
 
 const Jwt = require("jsonwebtoken");
 require("dotenv").config();
-const jwtKey = process.env.JWT_SECRET;
+const jwtKey = process.env.JWT_SECRET; 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//  API of Sing up
 app.post("/signup", async (req, resp) => {
   if (req.body.name && req.body.email && req.body.password) {
     const user = new userModel(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password;
+    let resultData = await user.save();
+    resultData = resultData.toObject();
+    delete resultData.password;
 
-    Jwt.sign({ result }, jwtKey, (err, token) => {
+    Jwt.sign({ resultData }, jwtKey, (err, token) => {
       if (err) {
         resp.send("Something went wrong. Please try again later!!")
       };
-      resp.send({result, auth: token });
+      resp.send({resultData, authToken: token });
     });
     
   } else {
     resp.send({ Result: "Name, email and password required for login!!" });
-  }
+  };
 });
+// API of Login
 app.post("/login", async (req, resp) => {
   if (req.body.email && req.body.password) {
-    const searchUser = await userModel.findOne(req.body).select("-password");
+    const userDetail = await userModel.findOne(req.body).select("-password");
 
-    if (searchUser) {
-      Jwt.sign({ searchUser }, jwtKey, (err, token) => {
+    if (userDetail) {
+      Jwt.sign({ userDetail }, jwtKey, (err, token) => {
         if (err) {
           resp.send({Result: "Something went wrong. Please try again later!!"});
         };
-        resp.send({searchUser, auth: token });
+        resp.send({userDetail, authToken: token });
       });
     } else {
       resp.send({ Result: "No user found!!" });
@@ -49,6 +51,7 @@ app.post("/login", async (req, resp) => {
     resp.send({ Result: "Email and password require for login!!" });
   };
 });
+// API of user account delete 
 app.delete("/account/:id", async (req, resp) => {
   const getResult = await userModel.deleteOne({ _id: req.params.id });
   if (getResult) {
@@ -58,6 +61,7 @@ app.delete("/account/:id", async (req, resp) => {
   }
 });
 
+// API of product add
 app.post("/add-product", async (req, resp) => {
   if ( req.body.name && req.body.price && req.body.brand && req.body.category && req.body.userId ) {
     const feedData = new productModel(req.body);
@@ -67,6 +71,7 @@ app.post("/add-product", async (req, resp) => {
     resp.send({ Result: "Please fill the all are requirement details!!" });
   }
 });
+// API of visible products of login/signup user only
 app.get("/products-of-user/:userId", async ( req, resp) => {
   const getData = await productModel.find({ userId: req.params.userId });
   if (getData.length > 0) {
@@ -75,6 +80,7 @@ app.get("/products-of-user/:userId", async ( req, resp) => {
     resp.send({ Result: "No products found!!" });
   }
 });
+// API of product delete
 app.delete("/products/:id", async (req, resp) => {
   const getResult = await productModel.deleteOne({ _id: req.params.id });
   if (getResult) {
@@ -83,7 +89,8 @@ app.delete("/products/:id", async (req, resp) => {
     resp.send({ Result: "Some thing went wrong!!" });
   }
 });
-app.get("/products/:id", async (req, resp) => {      // get for update the product
+// API of get product for update this product
+app.get("/products/:id", async (req, resp) => {      
   const getData = await productModel.findOne({ _id: req.params.id });
   if (getData) {
     resp.send(getData);
@@ -91,6 +98,7 @@ app.get("/products/:id", async (req, resp) => {      // get for update the produ
     resp.send({ Result: "No record found!!" });
   }
 });
+// API of update product
 app.put("/products/:id", async (req, resp) => {
   const getData = await productModel.updateOne(
     { _id: req.params.id },
@@ -100,6 +108,7 @@ app.put("/products/:id", async (req, resp) => {
     resp.send(getData);
   }
 });
+// API of product search
 app.get("/search/:key", async (req, resp) => {
   const getdata = await productModel.find({
     $or: [
@@ -110,10 +119,10 @@ app.get("/search/:key", async (req, resp) => {
   });
   resp.send(getdata);
 });
+// API of delete products of an user on an account delete
 app.delete("/products-of-user/:userId", async ( req, resp) => {
   const getData = await productModel.deleteMany({ userId: req.params.userId });
   resp.send(getData);
-  
 });
 
 app.listen(5000);
